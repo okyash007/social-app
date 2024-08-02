a4c2155b76e76966a8755f3d1f51ca79dc98b227
Yash Verma <120806297+okyash007@users.noreply.github.com>
2024-08-03 00:59:57 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ export const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://gitsta-frontend.vercel.app"],
+    origin: ["http://localhost:5173", "https://social.okyash.tech"],
     credentials: true,
   })
 );
